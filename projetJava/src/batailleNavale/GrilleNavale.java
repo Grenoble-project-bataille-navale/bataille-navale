e08d804d56a1e16193f0e6508599d2fe23564ffe
Marc-Arold <66696338+Marc-Arold@users.noreply.github.com>
2024-12-16 12:58:00 -0500

    Update GrilleNavale.java

--- projetJava/src/batailleNavale/GrilleNavale.java
+++ projetJava/src/batailleNavale/GrilleNavale.java
@@ -8,14 +8,16 @@ public class GrilleNavale {
 	private int nbTirsRecus;
 	
 	public GrilleNavale(int taille, int[] taillesNavires) {
-		if (taille <= 2 ) throw new IllegalArgumentException("Taille trop petite");
-		if (taillesNavires == null ) throw new IllegalArgumentException("Il faut un tableau....");
+		if (taille <= 2  ) throw new IllegalArgumentException("Taille trop petite");
+		if (taillesNavires == null  ) throw new IllegalArgumentException("Il faut un tableau....");
+		
 		this.taille = taille;
 		this.navires = new Navire [taillesNavires.length];
-		this.nbNavires = 0;
-		this.placementAuto(taillesNavires);
 		this.tirsRecu = new Coordonnee[taille*taille];
 		this.nbTirsRecus=0;
+		this.nbNavires = 0;
+		this.placementAuto(taillesNavires);
+		
 	}
 	public GrilleNavale(int taille, int nbNavires) {
 		if (nbNavires > taille) throw new IllegalArgumentException("Erreur Arguments");
@@ -26,14 +28,54 @@ public class GrilleNavale {
 	}
 	
 	public String toString() {
-
-		for (int i=0; i< this.taille; i++) {
-			for (int j =0; j< this.taille ; j++ ) {
-				Coordonnee c = new Coordonnee(i, j);
-				
+		StringBuffer sb = new StringBuffer("");
+		sb.append(initLettres());
+		for (int i = 0; i < taille ; i++) {
+			sb.append(i+1);
+			sb.append(' ');
+			for ( int j = 0; j < taille ;j++) {
+				Coordonnee courant = new Coordonnee(i,j);
+				if (estDansTirsRecus(courant)){
+					if(estTouche(courant)) {
+						sb.append('x');
+					}
+					else {
+						sb.append('O');
+					}
+				}
+				else {
+					if (navirePresent(courant))
+						sb.append('#');
+					else
+						sb.append('.');
+				}
+				sb.append(' ');
 			}
+			sb.append('\n');
 		}
-		return null;
+		return sb.toString();
+	}
+	private String initLettres() {
+		//methode outil pour le ToString
+		StringBuffer sb = new StringBuffer(" ");
+		for ( int i = 0; i < taille; i++) {
+			sb.append( (char) ('A'+i));
+			sb.append(' ');
+		}
+		sb.append('\n');
+		return sb.toString();
+		
+		
+	}
+		//méthode outil pour le ToSring
+	private boolean navirePresent(Coordonnee c) {
+		for (int i = 0; i < navires.length; i++) {
+			if ( navires[i].contient(c))
+				return true;
+		}
+	
+		return false;
+		
 	}
 	public int getTaille() {
 		return this.taille;
@@ -42,9 +84,9 @@ public class GrilleNavale {
 		
 		if(this.nbNavires >= this.navires.length) return false;
 		for (int i=0; i< navires.length; i++) {
-			if (n == null || navires[i].touche(n) || navires[i].chevauche(n)) return false;
+			if (navires[i] == null || navires[i].touche(n) || navires[i].chevauche(n)) continue;
 		}
-		navires[this.nbNavires+1] =n;
+		navires[this.nbNavires] =n;
 		this.nbNavires++;
 		return true;
 	}
@@ -52,17 +94,23 @@ public class GrilleNavale {
 	
 	public void placementAuto(int[] taillesNavires) {
 		for (int i=0; i<taillesNavires.length; i++) {
+			System.out.println(i);
 			boolean place=false;
 			while(!place) {
-				boolean[] estVerticale = {true, false, true, false};
+				System.out.println(place);
+				boolean[] estVerticale = {true, false};
 				int debutAlea = (int) (Math.random()* taille +1);
 				int lngAlea = (int) (Math.random()* taille +1);
-				if (lngAlea < taille - debutAlea  ) continue;
-				int indice = (int) (Math.random()*5);
+//				if (lngAlea > taille - debutAlea  ) {
+//					continue;
+//				};
+				int indice = (int) (Math.random()*2);
 				boolean v = estVerticale[indice];
 				Coordonnee C = new Coordonnee(debutAlea,lngAlea);
+				System.out.println(C);
 				Navire n = new Navire(C,lngAlea, v);
-				if(this.ajouterNavire(n)) {
+				if ((v && lngAlea > taille - C.getLigne()) || (!v && lngAlea> taille - C.getColonne())) continue;
+				if(ajouterNavire(n)) {
 					place=true;
 				}
 			}
@@ -70,13 +118,13 @@ public class GrilleNavale {
 		
 	}
 	private boolean estDansGrille(Coordonnee c) {
-		if (c.getColonne()<0 || c.getColonne() <0 || c.getColonne()>taille || c.getLigne() > taille) return false;
+		if (c.getColonne()<0 || c.getLigne() <0 || c.getColonne()>taille || c.getLigne() > taille) return false;
 		return true;
 	}
 	
 	private boolean estDansTirsRecus(Coordonnee c) {
 		if (!this.estDansGrille(c)) return false;
-		for (int i =0 ; i< tirsRecu.length; i++) {
+		for (int i =0 ; i< nbTirsRecus; i++) {
 			if(tirsRecu[i].equals(c)) return true;
 		}
 		return false;
@@ -92,11 +140,11 @@ public class GrilleNavale {
 	}
 	public boolean recoitTir(Coordonnee c) {
 		if (!(ajouteDansTirsRecus(c))) return false;
-//		for (int i = 0; i< navires.length; i++) {
-//			if (navires[i].estTouche(c)) return true;
-//		}
-		return estTouche(c);
-//		return false;
+		for (int i = 0; i< navires.length; i++) {
+			if (navires[i].recoitTir(c)) return true;
+		}
+		//return estTouche(c);
+		return false;
 	}
 	public boolean estTouche(Coordonnee c) {
 		for(int i=0; i<navires.length; i++) {
@@ -106,19 +154,28 @@ public class GrilleNavale {
 	}
 	
 	public boolean estALeau(Coordonnee c) {
-		return estDansTirsRecus(c) &&  recoitTir(c);
+		return  estDansTirsRecus(c) &&  estTouche(c);
 	}
 	
 	public boolean estCoule(Coordonnee c) {
+		for (int i=0; i<navires.length; i++) {
+			if (navires[i].estTouche(c) && navires[i].estCoule()) return true;
+		}
 		return false;
 	}
 	
-	public boolean perdu(Coordonnee c) {
-		return false;
+	public boolean perdu() {
+		for (int i=0; i<navires.length; i++) {
+			if(!(navires[i].estCoule())) return false;
+		}
+		return true;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] tailleNavires = {2,3,5};
+		GrilleNavale g = new GrilleNavale(10,tailleNavires);
+		System.out.println(g);
 
 	}
 
