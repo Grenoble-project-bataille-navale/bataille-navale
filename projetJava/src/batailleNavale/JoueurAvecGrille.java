68f65e2d0d139e6ca3a1e7e1fe29fdd65a5e29e5
Nicosmos <nicolasdijoud@yahoo.com>
2024-12-17 11:44:34 +0100

    ajout joueur grille

--- projetJava/src/batailleNavale/JoueurAvecGrille.java
+++ projetJava/src/batailleNavale/JoueurAvecGrille.java
@@ -3,11 +3,28 @@ package batailleNavale;
 public abstract class JoueurAvecGrille extends Joueur {
 	private GrilleNavale grille;
 
-	public JoueurAvecGrille(GrilleNavale g, String nom) {}
+	public JoueurAvecGrille(GrilleNavale g, String nom) {
+		super(g.getTaille(), nom);
+		this.grille = g;
+	}
 
-	public JoueurAvecGrille(GrilleNavale g) {}
+	public JoueurAvecGrille(GrilleNavale g) {
+		super(g.getTaille());
+		this.grille = g;
+	}
 
-	public int defendre(Coordonnee c) {}
+	public int defendre(Coordonnee c) {
+		grille.recoitTir(c);
+		if (grille.perdu())
+			return GAMEOVER;
+		else if (grille.estCoule(c))
+			return COULE;
+		else if (grille.estTouche(c))
+			return TOUCHE;
+		else {
+			return A_L_EAU;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
