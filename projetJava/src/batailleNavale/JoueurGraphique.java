8698f6bfac51759303fee642e36bd2a7052f453e
Marc-Arold <66696338+Marc-Arold@users.noreply.github.com>
2024-12-18 07:47:36 -0500

    Update JoueurGraphique.java

--- projetJava/src/batailleNavale/JoueurGraphique.java
+++ projetJava/src/batailleNavale/JoueurGraphique.java
@@ -1,1 +1,41 @@
+package batailleNavale;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+public class JoueurGraphique extends JoueurAvecGrille {
+private GrilleGraphique grilleTirs;
+public JoueurGraphique(GrilleNavaleGraphique grilleDefense, GrilleGraphique grilleTirs, String nom) {
+	super( grilleDefense,nom);
+	this.grilleTirs = grilleTirs;
+}
+public JoueurGraphique(GrilleNavaleGraphique grilleDefense,GrilleGraphique grilleTirs) {
+	super( grilleDefense);
+	this.grilleTirs = grilleTirs;
+	
+}
+public Coordonnee choixAttaque() {
+	return this.choixAttaque();
+}
+protected void retourDefense(Coordonnee c, int etat) {
+	switch (etat) {
+	case TOUCHE: JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché un navire en " + c);
+	break;
+	case COULE:	JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
+	break;
+	case GAMEOVER:				JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
+}
+}
+	protected void retourAttaque(Coordonnee c, int etat) {
+			Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
+			grilleTirs.colorie(c, couleur);
+			switch (etat) {
+			case TOUCHE: JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché un navire en " + c);
+			break;
+			case COULE:	JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
+			break;
+			case GAMEOVER:				JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
+}
+}
+}
