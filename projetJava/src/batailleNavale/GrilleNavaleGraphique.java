40adb4e46369fb84be13f6f9efd3c7bae2c00fd7
Marc-Arold <66696338+Marc-Arold@users.noreply.github.com>
2024-12-18 05:14:50 -0500

    Update GrilleNavaleGraphique.java

--- projetJava/src/batailleNavale/GrilleNavaleGraphique.java
+++ projetJava/src/batailleNavale/GrilleNavaleGraphique.java
@@ -1,1 +1,29 @@
+package batailleNavale;
+
+import java.awt.Color;
+
+public class GrilleNavaleGraphique  extends GrilleNavale {
+	private GrilleGraphique grille;
+	public GrilleNavaleGraphique(int taille) {
+		super(taille,0);
+		this.grille =  new GrilleGraphique(taille);
+	}
+	public GrilleGraphique getGrilleGraphique() {
+		return grille;
+	}
+	public boolean ajouteNavire(Navire n) {
+		return false;
+	}
+	public boolean recoitTir(Coordonnee c) {
+		 boolean tir = super.recoitTir(c);
+		 if (tir) {
+			 
+			 grille.colorie(c, Color.RED);
+		 } else {
+			 grille.colorie(c, Color.BLUE);
+		 }
+		 return tir;
+	}
+
+}
 
