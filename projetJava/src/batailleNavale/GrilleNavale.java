98a3d7760e2cf60bfe6295bf4f2d7bbe7b09963f
Nizijolas <nicolasdijoud@yahoo.com>
2024-12-18 15:14:18 +0100

    Update GrilleNavale.java

--- projetJava/src/batailleNavale/GrilleNavale.java
+++ projetJava/src/batailleNavale/GrilleNavale.java
@@ -1,48 +1,229 @@
 package batailleNavale;
 
-import java.awt.Color;
+public class GrilleNavale {
+	private Navire[] navires;
+	private int nbNavires;
+	private int taille;
+	private Coordonnee[] tirsRecus;
+	private int nbTirsRecus;
 
-public class GrilleNavaleGraphique  extends GrilleNavale {
-	private static int[] taillesNavire= {5,4,3,3,2};
-	private GrilleGraphique grille;
-	public GrilleNavaleGraphique(int taille) {
-		
-		super(taille,taillesNavire.length);
-		System.out.println("oui1");
-		this.grille =  new GrilleGraphique(taille);
-		System.out.println("oui2");
-		this.placementAuto(taillesNavire);
-		System.out.println("oui3");
+	public GrilleNavale(int taille, int[] taillesNavires) {
+		if (taille <= 2)
+			throw new IllegalArgumentException("Taille trop petite");
+		if (taillesNavires == null)
+			throw new IllegalArgumentException("Il faut un tableau....");
+
+		this.taille = taille;
+		this.navires = new Navire[taillesNavires.length];
+		this.tirsRecus = new Coordonnee[taille * taille];
+		this.nbTirsRecus = 0;
+		this.nbNavires = 0;
+		this.placementAuto(taillesNavires);
+
 	}
-	public GrilleGraphique getGrilleGraphique() {
-		return grille;
+
+	public GrilleNavale(int taille, int nbNavires) {
+		if (nbNavires > taille)
+			throw new IllegalArgumentException("Erreur Arguments");
+		this.taille = taille;
+		this.navires = new Navire[nbNavires];
+		this.nbNavires = 0;
+		this.tirsRecus = new Coordonnee[taille * taille];
 	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer("");
+		sb.append(initLettres());
+		for (int i = 0; i < taille; i++) {
+			if (i < 9)
+				sb.append(' ');
+			sb.append(i + 1);
+			sb.append(' ');
+			for (int j = 0; j < taille; j++) {
+				Coordonnee courant = new Coordonnee(i, j);
+				if (estDansTirsRecus(courant)) {
+					if (estTouche(courant)) {
+						sb.append('x');
+					} else {
+						sb.append('O');
+					}
+				} else {
+					if (navirePresent(courant))
+						sb.append('#');
+					else
+						sb.append('.');
+				}
+				sb.append(' ');
+			}
+			sb.append('\n');
+		}
+		return sb.toString();
+	}
+
+	private String initLettres() {
+		// methode outil pour le ToString
+		StringBuffer sb = new StringBuffer("   ");
+		for (int i = 0; i < taille; i++) {
+			sb.append((char) ('A' + i));
+			sb.append(' ');
+		}
+		sb.append('\n');
+		return sb.toString();
+
+	}
+
+	// méthode outil pour le ToSring
+	private boolean navirePresent(Coordonnee c) {
+		for (int i = 0; i < navires.length; i++) {
+			if (navires[i].contient(c))
+				return true;
+		}
+
+		return false;
+
+	}
+
+	public int getTaille() {
+		return this.taille;
+	}
+
 	public boolean ajouteNavire(Navire n) {
+		if (this.nbNavires > this.navires.length)
+			return false;
+		for (int i = 0; i < this.nbNavires; i++) {
+			if (navires[i].touche(n))
+				return false;
+			if (navires[i].chevauche(n)) {
+
+				return false;
+			}
+		}
+		navires[this.nbNavires] = n;
+		this.nbNavires++;
+		return true;
+	}
+
+	public void placementAuto(int[] taillesNavires) {
+		taillesNavires = sortTaillesNavires(taillesNavires);
+		for (int i = 0; i < taillesNavires.length; i++) {
+			if (taillesNavires[i] > taille)
+				throw new IllegalArgumentException("Un bateau est plus grand que la grille");
+			boolean place = false;
+			while (!place) {
+				System.out.println("oui");
+				boolean[] estVerticale = { true, false };
+				int indice = (int) (Math.random() * 2);
+				boolean estVertical = estVerticale[indice];
+				int ligneAlea = estVertical ? (int) (Math.random() * (taille-taillesNavires[i])) : (int) (Math.random()*taille); 
+				int colonneAlea = !estVertical ? (int) (Math.random() * (taille-taillesNavires[i])) : (int) (Math.random()*taille); 
+				Coordonnee c = new Coordonnee(ligneAlea, colonneAlea);
+				Navire n = new Navire(c, taillesNavires[i], estVertical);
+
+				if (ajouteNavire(n)) {
+					place = true;
+					System.out.println("oui");
+				}
+			}
+		}
+
+	}
+	private int[] sortTaillesNavires(int[] taillesNavires) {
+		//Méthode outil de trie car ranger les bateaux du plus grand au plus petit est plus rapide
+		for (int i=0; i< taillesNavires.length-1; i++) {
+			for (int j=0; j<taillesNavires.length-i-1; j++) {
+				if(taillesNavires[j]<taillesNavires[j+1]) {
+					int temp = taillesNavires[j+1];
+					taillesNavires[j+1] = taillesNavires[j];
+					taillesNavires[j] = temp;
+				}
+			}
+		}
 		
-		if(super.ajouteNavire(n)) {
-			if ( n.getDebut().getLigne() == n.getFin().getLigne() ) {
-				// On colorie à l'horizontale
-				for ( int i = n.getDebut().getColonne() ; i <= n.getFin().getColonne() ; i++ )
-					this.grille.colorie(new Coordonnee(n.getDebut().getLigne(), i), Color.GREEN);
-			}
-			else {
-				//On colorie à la verticale
-				for ( int i = n.getDebut().getLigne() ; i <= n.getFin().getLigne() ; i++ )
-					this.grille.colorie(new Coordonnee(i, n.getDebut().getColonne()), Color.GREEN);
-			}
-			return true;
+		return taillesNavires;
+	}
+
+	private boolean estDansGrille(Coordonnee c) {
+		return c.getColonne() >= 0 && c.getColonne() < taille && c.getLigne() >= 0 && c.getLigne() < taille;
+	}
+
+	private boolean estDansTirsRecus(Coordonnee c) {
+		if (c == null || !this.estDansGrille(c))
+			return false;
+		for (int i = 0; i < nbTirsRecus; i++) {
+			if (tirsRecus[i].equals(c))
+				return true;
 		}
 		return false;
 	}
+
+	private boolean ajouteDansTirsRecus(Coordonnee c) {
+		if ( c == null)
+			throw new IllegalArgumentException("coordonnee nulle");
+		if (estDansTirsRecus(c))
+			return false;
+		if (nbTirsRecus >= tirsRecus.length) {
+			throw new ArrayIndexOutOfBoundsException("Le tableau des tirs reçus est plein.");
+		}
+		tirsRecus[nbTirsRecus] = c;
+		nbTirsRecus++;
+
+		return true;
+	}
+
 	public boolean recoitTir(Coordonnee c) {
-		 boolean tir = super.recoitTir(c);
-		 if (tir) {
-			 
-			 grille.colorie(c, Color.RED);
-		 } else {
-			 grille.colorie(c, Color.BLUE);
-		 }
-		 return tir;
+		if (c == null || !(ajouteDansTirsRecus(c)))
+			return false;
+		for (int i = 0; i < navires.length; i++) {
+			if (navires[i].recoitTir(c))
+				return true;
+		}
+
+		return false;
+	}
+
+	public boolean estTouche(Coordonnee c) {
+		for (int i = 0; i < navires.length; i++) {
+			if (navires[i].estTouche(c))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean estALeau(Coordonnee c) {
+		return estDansTirsRecus(c) && !estTouche(c);
+	}
+
+	public boolean estCoule(Coordonnee c) {
+		for (int i = 0; i < navires.length; i++) {
+			if (navires[i].estTouche(c) && navires[i].estCoule())
+				return true;
+		}
+		return false;
+	}
+
+	public boolean perdu() {
+		for (int i = 0; i < navires.length; i++) {
+			if (!(navires[i].estCoule()))
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] tailleNavires = { 3,3,3};
+		GrilleNavale g = new GrilleNavale(10, tailleNavires);
+		Coordonnee a = new Coordonnee(1, 2);
+		while (!g.estCoule(a)) {
+			int i = (int) (Math.random() * (g.taille/2));
+		int j = (int) (Math.random() * (g.taille/2));
+		System.out.println("a");
+			a = new Coordonnee(i, j);
+			g.recoitTir(a);
+			g.ajouteDansTirsRecus(a);
+	}
+		System.out.println(g);
+
 	}
 
 }
