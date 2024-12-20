17bd81a79fee6919547f0ea1b775393fcd484b56
Nicosmos <nicolasdijoud@yahoo.com>
2024-12-20 11:30:29 +0100

    oui

--- projetJava/src/batailleNavale/JoueurGraphique.java
+++ projetJava/src/batailleNavale/JoueurGraphique.java
@@ -1,8 +1,13 @@
 package batailleNavale;
 
 import java.awt.Color;
+import java.io.File;
 
-
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.swing.JOptionPane;
 
 public class JoueurGraphique extends JoueurAvecGrille  {
@@ -24,8 +29,18 @@ protected void retourDefense(Coordonnee c, int etat) {
 //	case A_L_EAU: JOptionPane.showMessageDialog(grilleTirs, "Votre adversaire a tiré dans l'eau");
 //	break;
 	case TOUCHE: JOptionPane.showMessageDialog(grilleTirs, "Vous avez été touché en " + c);
+		
+		
+	
+	
 	break;
 	case COULE:	JOptionPane.showMessageDialog(grilleTirs, "Votre navire a coulé en " + c);
+	try {
+		ajouterSound("coulerSon.wav");
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	break;
 	case GAMEOVER:				JOptionPane.showMessageDialog(grilleTirs, "Vous avez Perdu!!!");
 	
@@ -38,10 +53,25 @@ protected void retourDefense(Coordonnee c, int etat) {
 //			case A_L_EAU: JOptionPane.showMessageDialog(grilleTirs, "Dans l'eau!");
 //			break;
 			case TOUCHE: JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché un navire en " + c);
+			
 			break;
 			case COULE:	JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
+			try {
+				ajouterSound("coulerSon.wav");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 			case GAMEOVER:				JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
 }
 }
+	
+	protected void ajouterSound(String chemin) throws Exception {
+		File f = new File("./src/batailleNavale/"+chemin);
+	    AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());  
+	    Clip clip = AudioSystem.getClip();
+	    clip.open(audioIn);
+	    clip.start();
+	}
 }
